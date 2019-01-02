5b563f7983915fc0db3a89218c5b2b5f590de3e1
Adrian Breazu <breazuadrian@gmail.com>
2019-01-02 12:37:59 +0200

    actuators cleanup

--- src/magenta/static/resources/js/actuators.js
+++ src/magenta/static/resources/js/actuators.js
@@ -2,40 +2,40 @@
 (function() {
     $('#grass_front').click(function() {
         if ($(this).attr('class') != "select-area") {
-            startRelay('A', 'True');
+            startRelay('A', 1);
             $(this).attr('class', 'select-area');
         } else {
-            startRelay('A', 'False');
+            startRelay('A', 0);
             $(this).attr('class', '');
         }
     });
 
     $('#grass_back').click(function() {
         if ($(this).attr('class') != "select-area") {
-            startRelay('B', 'True');
+            startRelay('B', 1);
             $(this).attr('class', 'select-area');
         } else {
-            startRelay('B', 'False');
+            startRelay('B', 0);
             $(this).attr('class', '');
         }
     });
 
     $('#flowers').click(function() {
         if ($(this).attr('class') != "select-area") {
-            startRelay('D', 'True');
+            startRelay('D', 1);
             $(this).attr('class', 'select-area');
         } else {
-            startRelay('D', 'False');
+            startRelay('D', 0);
             $(this).attr('class', '');
         }
     });
 
     $('#grass_side').click(function() {
         if ($(this).attr('class') != "select-area") {
-            startRelay('C', 'True');
+            startRelay('C', 1);
             $(this).attr('class', 'select-area');
         } else {
-            startRelay('C', 'False');
+            startRelay('C', 0);
             $(this).attr('class', '');
         }
     });
@@ -62,12 +62,12 @@
             else
                 $('#grass_back').attr('class', 'select-area');
 
-            if (response['Relay_C'] == 0)
+            if (response['Relay_D'] == 0)
                 $('#flowers').attr('class', '');
             else
                 $('#flowers').attr('class', 'select-area');
 
-            if (response['Relay_D'] == 0)
+            if (response['Relay_C'] == 0)
                 $('#grass_side').attr('class', '');
             else
                 $('#grass_side').attr('class', 'select-area');
@@ -88,9 +88,7 @@ function startRelay(relayId, status) {
         contentType: 'application/json',
         data: '{"Relay":\"' + relayId + '\", "Status":' + status + '}',
         async: false,
-        success: function(response) {
-
-        },
+        success: function(response) {},
         error: function(error) {
             console.log(error);
         }
